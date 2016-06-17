80d28bc950dbb104fcc320eec0479a7c80eb946a
Drew Funderburk <thatfunderburkguy@gmail.com>
2016-06-17 10:28:36 -0500

    Added checkCollision(Rectangle[] object1, Rectangle[] object2) to Collision.

--- src/World/Collision.java
+++ src/World/Collision.java
@@ -5,6 +5,13 @@ import java.awt.Rectangle;
 public class Collision {
 
    public boolean checkCollision(Rectangle[] object1, Rectangle[] object2) {
-      
+      for (int i = 0; i < object1.length; i++) {
+         for (int j = 0; j < object2.length; j++) {
+            if (object1[i].intersects(object2[j])) {
+               return true;
+            } else return false;
+         }
+      }
+      return false;
    }
 }
