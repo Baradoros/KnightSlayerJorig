0aea2b76af7c8f7d319400c4fe4010264ee2b51a
Drew Funderburk <thatfunderburkguy@gmail.com>
2016-06-23 22:11:13 -0500

    Added JavaDoc comments to Collision.java

--- src/World/Collision.java
+++ src/World/Collision.java
@@ -2,8 +2,19 @@ package World;
 
 import java.awt.Rectangle;
 
+/**
+ * Class containing methods for collision detection.
+ * @author Drew
+ *
+ */
 public class Collision {
 
+   /**
+    * Checks for intersection between two arrays of rectangles.
+    * @param object1
+    * @param object2
+    * @return
+    */
    public boolean checkCollision(Rectangle[] object1, Rectangle[] object2) {
       
       for (int i = 0; i < object1.length; i++) {
