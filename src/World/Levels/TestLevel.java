797f1c9f0999d181fecc1dba69f38550541eb257
Drew Funderburk <thatfunderburkguy@gmail.com>
2016-07-11 15:40:50 -0500

    Wiped TestLevel because of recent changes to Tile

--- src/World/Levels/TestLevel.java
+++ src/World/Levels/TestLevel.java
@@ -34,38 +34,6 @@ public class TestLevel extends Level {
    @Override
    public void init() {
       Assets.init();
-      // ADD USED TILES
-      // **********************************************************************
-      // TODO URGENT LOOK INTO BETTER WAY TO ORGANIZE THESE THAT ALLOWS
-      // SEARCHING BY
-      // NAME RATHER THAN INDEX.
-      // IMPLIMENT BEFORE CREATING ANYTHING OTHER THAN THE TEST LEVEL.
-      // AIN'T NOBODY GOT TIME TO REDO ALL THE LEVELS LATER.
-      // **********************************************************************
-      tiles.add(new Tile(Assets.bossCobblePlain1)); // 0
-      tiles.add(new Tile(Assets.bossCobblePlain2)); // 1
-      tiles.add(new Tile(Assets.bossCobblePlain3)); // 2
-      tiles.add(new Tile(Assets.bossCobbleCrater1)); // 3
-      tiles.add(new Tile(Assets.bossCobbleCrater2)); // 4
-      tiles.add(new Tile(Assets.bossCobbleCrater3)); // 5
-      tiles.add(new Tile(Assets.bossCobbleBlood1)); // 6
-      tiles.add(new Tile(Assets.bossCobbleBlood2)); // 7
-      tiles.add(new Tile(Assets.bossCobbleBlood3)); // 8
-      tiles.add(new Tile(Assets.bossCobbleBlood4)); // 9
-      tiles.add(new Tile(Assets.bossCobbleBlood5)); // 10
-
-      tiles.add(new Tile(Assets.lava1)); // 11
-      tiles.add(new Tile(Assets.lava2)); // 12
-      tiles.add(new Tile(Assets.lava3)); // 13
-      tiles.add(new Tile(Assets.lava4)); // 14
-
-      tiles.add(new Tile(Assets.darkSquareStone1)); // 15
-      tiles.add(new Tile(Assets.darkSquareStone2)); // 16
-      tiles.add(new Tile(Assets.darkSquareStone3)); // 17
-      tiles.add(new Tile(Assets.darkSquareStone4)); // 18
-      // SET LAVA AND SQUARE STONE TILES SOLID
-      for (int i = 11; i < 18; i++)
-         tiles.get(i).setSolid(true);
 
       // INITIALIZE THE PLAYER
       player = new Player(5, 5);
@@ -89,22 +57,6 @@ public class TestLevel extends Level {
 
    // THE BIG ONE
    private void buildLevel(Graphics2D g) {
-      // FLOOR
-      for (int i = 0; i < GamePanel.WIDTH / Assets.SCALE; i++) {
-         for (int j = 0; j < GamePanel.HEIGHT / Assets.SCALE; j++) {
-            tiles.get(0).drawTile(g, i, j);
-         }
-      }
-
-      // BORDER
-      LevelBuilder.drawBox(g, tiles.get(15), 0, 0,
-            GamePanel.WIDTH / Assets.SCALE, GamePanel.HEIGHT / Assets.SCALE);
-
-      // CENTER BOX
-      LevelBuilder.drawBox(g, tiles.get(15), 10, 7, 5, 5);
-      LevelBuilder.drawArea(g, tiles.get(11), 11, 8, 3, 3);
-      LevelBuilder.drawBox(g, tiles.get(15), 8, 5, 9, 9);
-      LevelBuilder.drawBox(g, tiles.get(11), 9, 6, 7, 7);
 
    }
    
